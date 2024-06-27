749da16424680ee3e7b382ed47efbec9d45683a4
vivinkv6 <vivinkv0@gmail.com>
2024-06-27 17:06:07 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,25 +33,35 @@ export default function Home() {
   }, [genre, searchMovie]);
 
   if (!movies) {
-    return <h1>No Movies List</h1>;
+    return (
+      <div className="bg-gray-950 h-[100dvh] w-[100%] flex justify-center items-center">
+        <header
+          className={`bg-gray-950 text-white p-4 md:w-1/4 text-center text-5xl max-sm:text-xl font-bold `}
+        >
+          MOVIE DASHBOARD
+        </header>
+      </div>
+    );
   }
 
   return (
     <div className="min-h-screen bg-gray-950 text-white">
-      <header className={`bg-gray-950 p-4 md:w-1/4 text-center text-3xl font-bold md:fixed md:top-0 md:left-0 `}>
+      <header
+        className={`bg-gray-950 p-4 md:w-1/4 text-center text-3xl font-bold md:fixed md:top-0 md:left-0 `}
+      >
         MOVIE DASHBOARD
       </header>
       <div className="flex flex-col md:flex-row ">
         <Nav />
-       
-          {movies.length < 1 && (
-             <div className="flex flex-row h-[100dvh] w-full justify-center items-center">
+
+        {movies.length < 1 && (
+          <div className="flex flex-row h-[100dvh] w-full justify-center items-center">
             <div className="border-2 border-orange-500 p-5 rounded-md">
               <h1>{searchMovie}: Movies not found</h1>
             </div>
-            </div>
-          )}
-       
+          </div>
+        )}
+
         <main className="w-full md:w-3/4 p-4 md:absolute md:top-0 md:right-0  bg-gray-950">
           <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-4 max-sm:place-items-center">
             {movies.map((movie: Movie) => (
