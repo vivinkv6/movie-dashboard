df7bfc1a255957b678adbae4acfea5037b22b185
vivinkv6 <vivinkv0@gmail.com>
2024-06-29 10:21:44 +0530

    update

--- src/app/components/SkeletonCard.tsx
+++ src/app/components/SkeletonCard.tsx
@@ -11,7 +11,7 @@ const SkeletonCard = () => {
         className={`min-w-full max-sm:mx-20 p-4 rounded-lg border-2 border-gray-500`}
       >
         <div className="mb-2 rounded ">
-          <div className="w-full h-64 bg-gray-700 rounded-md bg-gradient-to-r from-gray-700 via-gray-500 to-gray-700 bg-size-200 bg-pos-0 animate-shimmer"></div>
+          <div className="w-full h-80 bg-gray-700 rounded-md bg-gradient-to-r from-gray-700 via-gray-500 to-gray-700 bg-size-200 bg-pos-0 animate-shimmer"></div>
           <div className="mt-2 bg-gray-700 h-6 rounded w-3/4 mx-auto bg-gradient-to-r from-gray-700 via-gray-500 to-gray-700 bg-size-200 bg-pos-0 animate-shimmer"></div>
           <div className="mt-2 bg-gray-700 h-4 rounded w-1/2 mx-auto bg-gradient-to-r from-gray-700 via-gray-500 to-gray-700 bg-size-200 bg-pos-0 animate-shimmer"></div>
         </div>
