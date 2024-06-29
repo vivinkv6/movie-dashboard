39a5ef00e15ca91ec8899c67d8a1fec2a1ff7b2f
vivinkv6 <vivinkv0@gmail.com>
2024-06-29 10:14:52 +0530

    add border

--- src/app/components/SkeletonCard.tsx
+++ src/app/components/SkeletonCard.tsx
@@ -8,7 +8,7 @@ const SkeletonCard = () => {
     return (
       <div
         key={index}
-        className={`min-w-full max-sm:mx-20 p-4 rounded-lg `}
+        className={`min-w-full max-sm:mx-20 p-4 rounded-lg border-2 border-gray-500`}
       >
         <div className="mb-2 rounded ">
           <div className="w-full h-64 bg-gray-700 rounded-md bg-gradient-to-r from-gray-700 via-gray-500 to-gray-700 bg-size-200 bg-pos-0 animate-shimmer"></div>
